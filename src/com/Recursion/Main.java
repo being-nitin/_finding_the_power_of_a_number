429546333f67e04d7f7f78c7ea22f768051cde35
Nitin <nsinghboss02@gmail.com>
2022-01-19 00:34:26 +0530

    Initial commit

---
+++ src/com/Recursion/Main.java
@@ -0,0 +1,22 @@
+package com.Recursion;
+import java.util.Scanner;
+public class Main {
+
+    public static void main(String[] args) {
+	/*
+	Finding the power of a number without multiplying it with n times.
+	 */
+        Scanner sc = new Scanner(System.in);
+        int power = 6;
+        int base = 3;
+        int ans = 1;
+        while (power>0){
+            if((power & 1)==1){
+                ans = ans*base;
+            }
+            base*=base;
+            power = power >> 1;
+        }
+        System.out.println(ans);
+    }
+}
